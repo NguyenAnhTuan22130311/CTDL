a23439b91aaff4c4859c0f8c7a5d2ddd26e29485
NguyenAnhTuan22130311 <146004230+NguyenAnhTuan22130311@users.noreply.github.com>
2023-09-27 18:41:35 +0700

    Update MyArray.java

--- MyArray.java
+++ MyArray.java
@@ -33,12 +33,69 @@ public class MyArray {
 		return Arrays.copyOf(temp, j);
 	}
 
+	public int[] getMissingValues() {
+		// Tìm giá trị lớn =, nhỏ nhất
+		int min = array[0];
+		int max = array[0];
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] < min)
+				min = array[i];
+			if (array[i] > max)
+				max = array[i];
+		}
+
+		int[] missingValues = new int[max - min + 1 - array.length];
+		int index = 0;
+
+		for (int i = min; i <= max; i++) {
+			boolean found = false;
+			for (int j = 0; j < array.length; j++) {
+				if (array[j] == i) {
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				missingValues[index++] = i;
+			}
+		}
+		return missingValues;
+	}
+
+	public int[] fillMissingValues(int k) {
+		int n = array.length;
+
+		for (int i = 0; i < n - 1; i++) {
+			if (array[0] == -1) {
+				array[0] = (array[i + 1] + array[i + 2] + array[i + 3]) / k;
+			}
+			if (array[n - 1] == -1) {
+				array[n - 1] = (array[n - 1] + array[n - 2] + array[n - 3]) / k;
+			} else if (array[i] == -1) {
+				if (array[i - 1] + array[i - 2] + array[i + 1] > array[i + 1] + array[i + 2] + array[i - 1]) {
+					array[i] = (array[i + 1] + array[i + 2] + array[i - 1]) / k;
+				} else {
+					array[i] = (array[i - 1] + array[i - 2] + array[i + 1]) / k;
+				}
+
+			}
+		}
+
+		return array;
+	}
+
 	public static void main(String[] args) {
 		MyArray myArray1 = new MyArray(new int[] { 1, 2, 3 });
 		MyArray myArray2 = new MyArray(new int[] { 1, 3, 5, 1, 3, 7, 9, 8 });
+		MyArray myArray3 = new MyArray(new int[] { 10, 11, 12, 13, 14, 16, 17, 19, 20 });
+		MyArray myArray4 = new MyArray(new int[] { 10, 11, 12, -1, 14, 10, 17, 19, 20 });
 		int[] mirrorArray = myArray1.mirror();
 		int[] removeDuplicates = myArray2.removeDuplicates();
+		int[] getMissingValues = myArray3.getMissingValues();
+		int[] fillMissingValues = myArray4.fillMissingValues(3);
 		System.out.println(Arrays.toString(mirrorArray));
-		System.out.print(Arrays.toString(removeDuplicates));
+		System.out.println(Arrays.toString(removeDuplicates));
+		System.out.println(Arrays.toString(getMissingValues));
+		System.out.println(Arrays.toString(fillMissingValues));
 	}
 }
