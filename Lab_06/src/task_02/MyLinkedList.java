4757a1f73807229bc9a22fe09b0228dd61ba04af
Nguyen Anh Tuan <146004230+NguyenAnhTuan22130311@users.noreply.github.com>
2023-10-30 22:49:28 +0700

    Update MyLinkedList.java

--- Lab_06/src/task_02/MyLinkedList.java
+++ Lab_06/src/task_02/MyLinkedList.java
@@ -1,5 +1,20 @@
 package task_02;
 
+public class Node<E> {
+			private E data;
+			private Node<E> next;
+
+			public Node(E data) {
+				this.data = data;
+			}
+
+			public Node(E data, Node<E> next) {
+				this.data = data;
+				this.next = next;
+			}
+
+		}
+
 public class MyLinkedList {
 	public class SinglyLinkedList<E> {
 		private Node<E> head = null;
@@ -91,21 +106,6 @@ public class MyLinkedList {
 			tail.next = null;
 			size--;
 			return removedData;
-		}
-
-		public class Node<E> {
-			private E data;
-			private Node<E> next;
-
-			public Node(E data) {
-				this.data = data;
-			}
-
-			public Node(E data, Node<E> next) {
-				this.data = data;
-				this.next = next;
-			}
-
 		}
 	}
 }
