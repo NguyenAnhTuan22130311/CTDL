07889ad8d64ebd8555822e1ea72c8c418686635f
Nguyen Anh Tuan <146004230+NguyenAnhTuan22130311@users.noreply.github.com>
2023-11-13 23:15:16 +0700

    Update TestReadFile.java

--- Lab_08/src/MapProject/TestReadFile.java
+++ Lab_08/src/MapProject/TestReadFile.java
@@ -8,12 +8,23 @@ import java.util.Scanner;
 import java.util.function.Predicate;
 
 public class TestReadFile {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		MyWordCountApp myWordCount = new MyWordCountApp();
 		myWordCount.loadData();
 		System.out.println(myWordCount.countUnique());
 		myWordCount.printWordCounts();
 		myWordCount.printWordCountsAlphabet();
-		
+
+		TextAnalyzer text = new TextAnalyzer();
+		try {
+			text.load("Data/short");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		text.add("IT", 2);
+		text.displayWords();
+		text.displayText();
+		System.out.println(text.mostFrequentWord());
 	}
 }
